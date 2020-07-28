3fcf8adfc683e7d45b24250cee0a235cf60e9b81
Neil <neil.ong31@gmail.com>
2020-07-28 10:50:49 -0400

    Navbar and routes added

--- BTN710-Website/src/app/app-routing.module.ts
+++ BTN710-Website/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { ReportComponent } from './report/report.component';
+import { VideoComponent } from './video/video.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'report', component: ReportComponent},
+  { path: 'video', component: VideoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- BTN710-Website/src/app/app.module.ts
+++ BTN710-Website/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { ReportComponent } from './report/report.component';
+import { VideoComponent } from './video/video.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    ReportComponent,
+    VideoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]

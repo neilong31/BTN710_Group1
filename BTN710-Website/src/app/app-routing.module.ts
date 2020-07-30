318f8343f947104c74af1b1cb339ed0eae151216
Neil <neil.ong31@gmail.com>
2020-07-30 18:34:50 -0400

    Router Guard

--- BTN710-Website/src/app/app-routing.module.ts
+++ BTN710-Website/src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component'
 import { ReportComponent } from './report/report.component';
 import { VideoComponent } from './video/video.component';
-
+import * as auth from './auth.service';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
-  { path: 'report', component: ReportComponent},
-  { path: 'video', component: VideoComponent}
+  { path: 'report', component: ReportComponent, canActivate: [AuthGuard]},
+  { path: 'video', component: VideoComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
